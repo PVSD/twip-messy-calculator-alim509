626d6b99736b1ad3098eb79cd7b16f4bb9f47932
al5091 <angelovinolim@gmail.com>
2018-09-11 11:22:55 -0400

    Calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Test {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("Welcome the Limsane Calculator!");
+        System.out.println("What would like to do (+, -, *, /)?");
+        String operator = kbInput.next();
+
+        do {
+            if (operator != "+" || operator != "-" || operator != "*" || operator != "/");
+            {
+                System.out.println("That is not an operator! Pick either add, subtract, multiply, or divide");
+            }
+        } while (operator != "+" || operator != "-" || operator != "*" || operator != "/");
+
+        if (operator == "+") {
+            operator = "add";
+        }
+            System.out.println("What would like to " + operator + " together? Pick one number and press enter, then pick another number and press enter again.");
+        }
+
     }
 }
